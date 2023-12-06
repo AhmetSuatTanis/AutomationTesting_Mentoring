63368ba2ec0338a26fac61da2a30e3d032304870
Ahmet Suat Tanış <ahmetsuattanis@gmail.com>
2023-12-06 22:43:42 +0300

    5 aralık ilk commit

---
+++ src/test/java/com/kraft/tests/day02_WebDriverBasics/_1_A_UsingChromiumChrome.java
@@ -0,0 +1,9 @@
+package com.kraft.tests.day02_WebDriverBasics;
+
+public class _1_A_UsingChromiumChrome {
+    public static void main(String[] args) {
+
+        System.out.println("COMMIT");
+
+    }
+}
