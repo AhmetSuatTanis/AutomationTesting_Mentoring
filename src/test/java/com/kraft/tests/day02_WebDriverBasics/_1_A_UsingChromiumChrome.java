068185b0f4d1e1074809f06cf801e5a72a69fae5
AhmetSuatTanis <146131615+AhmetSuatTanis@users.noreply.github.com>
2023-12-06 22:54:32 +0300

    Update _1_A_UsingChromiumChrome.java

--- src/test/java/com/kraft/tests/day02_WebDriverBasics/_1_A_UsingChromiumChrome.java
+++ src/test/java/com/kraft/tests/day02_WebDriverBasics/_1_A_UsingChromiumChrome.java
@@ -3,7 +3,6 @@ package com.kraft.tests.day02_WebDriverBasics;
 public class _1_A_UsingChromiumChrome {
     public static void main(String[] args) {
 
-        System.out.println("COMMIT");
 
     }
 }
